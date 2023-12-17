4baaff974d7e0a5fe5025b25db1c1b7dce28899c
UnkaisMagiqus <unkaismagiqus@gmail.com>
2023-12-17 13:33:55 -0500

    added new installs and cloudinary feature to the server backend

--- BirdSpotter/server/data/posts.js
+++ BirdSpotter/server/data/posts.js
@@ -1,6 +1,14 @@
 import {posts} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
 import * as helpers from '../helpers.js';
+import * as cloud from '../utils/cloudinary.js'
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+import {dirname} from 'path';
+import { type } from 'os';
+const __dirname = dirname(__filename);
 
 export const create = async (userId, title, imageUrl, description, location, coordinates) => {
 	title = helpers.isValidString(title, "Title");
@@ -9,15 +17,73 @@ export const create = async (userId, title, imageUrl, description, location, coo
 	}
 
 	if (!ObjectId.isValid(userId)) {throw new Error("Error: invalid object ID.")};
+	let Allimages = imageUrl //req.files.imageInput
+	let paths = [];
 	imageUrl = helpers.isValidString(imageUrl);
 	description = helpers.isValidString(description);
 	location = helpers.isValidString(location);
 	coordinates = helpers.isValidString(coordinates);
 
+	//IMAGE
+	//----------------------
+	if (Array.isArray(Allimages)){
+		for(let x of req.files.imageInput){
+			if(x.mimetype === "image/jpeg" || x.mimetype === "image/png" || x.mimetype === "image/jpg" || x.mimetype === "jpeg/jpg" || x.mimetype === "jpg/jpeg"){
+				const image = x;
+			const writeStream = fs.createWriteStream(path.join(__dirname, '..', 'uploads', image.name));
+			paths.push(path.join(__dirname, '..', 'uploads', image.name));
+			await writeStream.write(image.data);
+			await writeStream.end(); 
+			}
+			else{
+				let pathway = path.join(__dirname, '..', 'uploads');
+				fs.readdir(pathway, (err, files) => {
+					if (err) throw err;
+					for( let x of files){
+						if (x != "258.png"){
+						let filepath = path.join(pathway, x);
+						fs.unlink(filepath, err => {
+							if (err) throw err;
+						});
+					}
+					}
+				});
+			}
+		}
+	}
+	else{
+			if(Allimages.mimetype === "image/jpeg" || Allimages.mimetype === "image/png" || Allimages.mimetype === "image/jpg" || Allimages.mimetype === "jpeg/jpg" || Allimages.mimetype === "jpg/jpeg"){
+				const image = Allimages;
+				let x = path.join(__dirname, '..', 'uploads', image.name);
+				const writeStream = fs.createWriteStream(x);
+				paths.push(path.join(__dirname, '..', 'uploads', image.name));
+				await writeStream.write(image.data);
+				await writeStream.end();
+			}
+			else{
+				let pathway = path.join(__dirname, '..', 'uploads');
+			//console.log(listing);
+			fs.readdir(pathway, (err, files) => {
+				if (err) throw err;
+				for( let x of files){
+					if (x != "258.png"){
+					let filepath = path.join(pathway, x);
+					fs.unlink(filepath, err => {
+						if (err) throw err;
+					});
+				}
+				}
+			});
+			}
+		
+	}
+	let images = await cloud.uploadImage(paths);
+	//----------------------
+
 	let newPost= {
 		userId: userId,
 		title: title,
-		imageUrl: imageUrl,
+		imageUrl: images,
 		description: description,
 		location: location,
 		coordinates: coordinates,
@@ -29,6 +95,23 @@ export const create = async (userId, title, imageUrl, description, location, coo
 	if (!insertInfo.acknowledged || !insertInfo.insertedId) {throw new Error("Error: unable to add post.")}
 	const newId = insertInfo.insertedId.toString();
 	const post = await get(newId);
+	
+	//removing images
+	//--------------------
+	let pathway = path.join(__dirname, '..', 'uploads');
+                        fs.readdir(pathway, (err, files) => {
+                            if (err) throw err;
+                            for( let x of files){
+                                if (x != "258.png"){
+                                let filepath = path.join(pathway, x);
+                                fs.unlink(filepath, err => {
+                                    if (err) throw err;
+                                });
+                            }
+                            }
+                        });
+	//--------------------
+
 	return post;
 };
 
