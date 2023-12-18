8744a25eb41590b8e5a1b18c69acea8ff1d4f8bb
lconcann <lconcann@stevens.edu>
2023-12-17 19:46:34 -0500

    Working leaflet Map with post examples

--- BirdSpotter/client/src/components/App.tsx
+++ BirdSpotter/client/src/components/App.tsx
@@ -6,6 +6,7 @@ import NotFound from './NotFound'
 import Login from "./Login"
 import Logout from "./Logout"
 import Profile from "./Profile"
+import MainMap from "./Map"
 
 function App() {
 
@@ -24,12 +25,16 @@ function App() {
             <NavLink className='navlink' to='/post'>Post a Sighting!</NavLink>
             <br/>
             <NavLink className='navlink' to='/profile'>View Profile Info</NavLink>
+            <br/>
+            <NavLink className='navlink' to='/map'>View Map</NavLink>
           </nav>
+          <br/> <br/>
         </header>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/post' element={<Post/>}/>
           <Route path='/profile' element={<Profile/>}/>
+          <Route path='/map' element={<MainMap/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
       </div>
