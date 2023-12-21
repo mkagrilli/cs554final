a390cec25755455646943ae4efdd1064d7647fe5
UnkaisMagiqus <unkaismagiqus@gmail.com>
2023-12-21 07:57:23 -0500

    Post form

--- BirdSpotter/client/src/components/Post.tsx
+++ BirdSpotter/client/src/components/Post.tsx
@@ -1,11 +1,119 @@
-import './App.css'
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
-function Post() {
-    return (
-        <div className='postPage'>
-            <p>Page to post Bird Sightings</p>
-        </div>
-    )
+
+interface FormData {
+  title: string;
+  desc: string;
+  image: File | null;
+  coordinates: [number, number] | null;
 }
 
-export default Post;
+const LeafletMap: React.FC<{ onCoordinatesChange: (coordinates: [number, number]) => void }> = ({
+  onCoordinatesChange,
+}) => {
+  const [position, setPosition] = useState<[number, number]>([0, 0]);
+
+  const handleMarkerDragEnd = (event: L.LeafletEvent) => {
+    const marker = event.target.getLatLng();
+    setPosition([marker.lat, marker.lng]);
+    onCoordinatesChange([marker.lat, marker.lng]);
+  };
+
+  return (
+    <>
+      <Marker position={position} draggable={true} eventHandlers={{ dragend: handleMarkerDragEnd }}>
+        <Popup>Drag me to select location</Popup>
+      </Marker>
+    </>
+  );
+};
+
+const MyForm: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    title: '',
+    desc: '',
+    image: null,
+    coordinates: null,
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+    setFormData({ ...formData, image: file });
+  };
+
+  const handleCoordinatesChange = (coordinates: [number, number]) => {
+    setFormData({ ...formData, coordinates });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const form = new FormData();
+      form.append('title', formData.title);
+      form.append('desc', formData.desc);
+      if (formData.image) {
+        form.append('image', formData.image);
+      }
+
+      if (formData.coordinates) {
+        form.append('latitude', formData.coordinates[0].toString());
+        form.append('longitude', formData.coordinates[1].toString());
+      }
+
+      const response = await axios.post<{ data: any }>('http://localhost:3000/posts/newpost', form, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log('Backend response:', response.data);
+    } catch (error) {
+      console.error('Error sending data to backend:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input type="text" name="title" value={formData.title} onChange={handleInputChange} />
+      </label>
+      <br />
+      <br />
+      <label>
+        Description:
+        <input type="text" name="desc" value={formData.desc} onChange={handleInputChange} />
+      </label>
+      <br />
+      <br />
+      <MapContainer center={[0, 0]} zoom={2} style={{ height: '300px', width: '100%' }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LeafletMap onCoordinatesChange={handleCoordinatesChange} />
+      </MapContainer>
+      {formData.coordinates && (
+        <p>
+          Selected Coordinates: {formData.coordinates[0]}, {formData.coordinates[1]}
+        </p>
+      )}
+
+      <br />
+      <br />
+      <label>
+        Image:
+        <input type="file" name="image" accept="image/*" onChange={handleImageChange} />
+      </label>
+      <br />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default MyForm;
--- BirdSpotter/server/routes/posts.js
+++ BirdSpotter/server/routes/posts.js
@@ -1,11 +1,15 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import axios from 'axios';
+import multer from 'multer';
+
+const upload = multer();
 const router = Router();
 
-import * as helpers from '../helpers.js';
 import * as posts from '../data/posts.js';
-
 import dotenv from 'dotenv';
 dotenv.config();
+var api_key = 'cf69ccfea8804bfa99abb6fe78e8f6f0';
+
 
 router.route('/').get(async (req, res) => {
     try {
@@ -15,24 +19,51 @@ router.route('/').get(async (req, res) => {
     }    
 });
 
-// router.route('/newpost').post(async (req, res) => {
-//     try {
-//         let postInfo = req.body;
-
-//         const post = await posts.create()
-//         return res.status(200).json({data: await users.get(req.params.id)});
-//     } catch (e) {
-//         return res.status(400).json({error: e.message});
-//     }
-// });
-
-router.route('/:id').get(async (req, res) => {
+router.route('/newpost').post(upload.single('image'), async (req, res) => {
     try {
-        req.params.id = helpers.isValidString(req.params.id, "User ID");
-        return res.status(200).json({data: await posts.get(req.params.id)});
+        console.log(req.body);
+        const { title, desc } = req.body; // Remove coordinates from here
+        const image = req.file;
+    
+        const coordinates = [parseFloat(req.body.latitude), parseFloat(req.body.longitude)]; // Parse coordinates
+    
+        console.log('Received form data:', { title, desc, coordinates });
+        console.log('Received image:', image);
+  
+      console.log('Received form data:', { title, desc });
+      console.log('Received image:', image);
+  
+      let latitude = coordinates[0];
+      let longitude = coordinates[1];
+      let query = latitude + ',' + longitude;
+      var api_url = 'https://api.opencagedata.com/geocode/v1/json';
+  
+      var request_url =
+        api_url +
+        '?' +
+        'key=' +
+        api_key +
+        '&q=' +
+        encodeURIComponent(query) +
+        '&pretty=1' +
+        '&no_annotations=1';
+  
+      const response = await axios.get(request_url);
+  
+      if (response.status === 200) {
+        const data = response.data;
+        let location = data.results[0].components.city + ', ' + data.results[0].components.country;
+        const post = await posts.create(id, title, image, desc, location, [longitude, latitude]);
+        return res.status(200).json({ data: post });
+      } else {
+        console.log('Unable to geocode! Response code: ' + response.status);
+        console.log('Error message: ' + response.data.status.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
     } catch (e) {
-        return res.status(400).json({error: e.message});
+      console.error('Error processing request:', e);
+      return res.status(400).json({ error: e.message });
     }
-});
+  });
 
 export default router;
